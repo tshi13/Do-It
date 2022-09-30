d39e3c7380f550a4cd4b28981a15cd6dd90d31bc
Jonathan Bakhit <jbakhit1@jh.edu>
2022-09-29 23:09:53 -0400

    Setup Browser Routing and Basic Navi
    Created the Navi Bar, created placeholder for Footer and Home and setup Browser Routing!

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import CssBaseline from '@mui/material/CssBaseline';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
