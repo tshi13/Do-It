827b3399ac67f56ef5a7aa56fa17805de148413a
Jonathan Bakhit <jbakhit1@jh.edu>
2022-09-30 17:25:35 -0400

    React-modal basic design created

---
+++ frontend/src/components/TaskModal.js
@@ -0,0 +1,72 @@
+import { style } from '@mui/system';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+
+export default function TaskModal() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const styleSheet = {
+    circle: {
+        borderRadius: '50%',
+        margin: 'auto',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputStyle: {
+        width: '100%',
+        height: '30px',
+        backgroundColor: 'lightgray',
+        borderRadius: '5px',
+        border: 'none',
+        marginBottom: '5%',
+    }
+
+    
+  }
+
+  return (
+    <div>
+        <Button variant="primary" onClick={handleShow} style ={styleSheet.circle}>
+            +
+        </Button>
+
+        <Modal
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+        >
+            <Modal.Header closeButton>
+            <Modal.Title>Add Task</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <form>
+                <div className="form-group">
+                    <input type="text" placeholder="Task Name" style ={styleSheet.inputStyle} />
+                    <input type="text" placeholder="Task Description" style ={styleSheet.inputStyle} />
+                    <input type="text" placeholder="Coins Per Task"  style ={styleSheet.inputStyle}/>
+                    <input type="text" placeholder="Task Due Date" style ={styleSheet.inputStyle} />
+                </div>
+
+            </form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary">Confirm</Button>
+            </Modal.Footer>
+        </Modal>
+      </div>
+  );
+}
--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
-
-
+import TaskModal from "../components/TaskModal";
 
 export default class Home extends Component {
     render() {
         return (
         <div>
+            <div className="taskModal">
+                <TaskModal/>
+            </div>
             <h1>Home</h1>
+            <button onClick={() => {}}> 
+                Login
+            </button>
         </div>
         );
     }
