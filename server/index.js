91b43e9a0916af7d532cfbc6ec41708f7ef0a15d
tshi13 <tshi13@jh.edu>
2022-10-01 10:30:53 -0400

    /addTask API

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const db = require("./db");
 const User = require("./schemaModels/User");
+const Task = require("./schemaModels/Task");
+
 const router = express.Router();
 var bodyParser = require('body-parser');
 const Group = require("./schemaModels/Group");
@@ -29,8 +31,8 @@ app.get("/", (req, res) => {
 
 
 app.post("/createUser", (req,res) =>{ //creates new user
-	const {name,coins,taskList} = req.body;
-		const data = User.create({name,coins,taskList})
+	const {name,coins,taskIDList} = req.body;
+		const data = User.create({name,coins,taskIDList})
 			.then((data) => {
 			console.log(data); //prints created user object
 			res.send(data);
@@ -40,6 +42,32 @@ app.post("/createUser", (req,res) =>{ //creates new user
 				.status(500)
 				.send({ message: "Error creating user with name: " + name })
 			})
+})
+
+app.put("/addTask", (req,res) => { //creates a new task and adds the coresponding objectID to User taskIDList
+	const {userID,taskName,time,coinsEntered} = req.body;
+	let taskID;
+	let newTaskIDList;
+	let newCoinBalance;
+	const task = Task.create({userID, taskName,time,coinsEntered})
+	.then((data) => {
+		taskID = data._id;
+		res.send(data);
+	})
+	.then(() => {
+		return User.findById(userID);
+	})
+	.then((user) => {
+		newTaskIDList = user.taskIDList;
+		newCoinBalance = user.coins - coinsEntered;
+		newTaskIDList.push(taskID);
+		return User.findOneAndUpdate({ _id: userID}, {taskIDList:newTaskIDList, coins:newCoinBalance});
+	})
+	.catch((err) => {
+		res
+		.status(500)
+		.send({ message: "Error creating task with name: " + taskName })
+	})
 })
 
 // please do not delete commented code out yet. This is for when we want to conenct the objectIDs in database
@@ -77,7 +105,6 @@ app.get("/tasks",(req,res) => { //gets all tasks that an _id has
         .send({ message: "Error retrieving tasks with id: " + _id });
     });	
 })
-
 
 
 app.listen(port, () => {
--- server/schemaModels/User.js
+++ server/schemaModels/User.js
@@ -1,15 +1,9 @@
 const mongoose = require("mongoose");
 
-const TaskSchema = new mongoose.Schema({
-	taskName: {type: String, required: true},
-	time: {type: Number, required: true},
-	coinsEntered: {type: Number, required: true}
-});
-
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
 	coins: {type: Number, required: true},
-	taskList: {type: [TaskSchema], required: true}
+	taskIDList: {type: [String], required: true}
 });
 
 const User = mongoose.model("User", UserSchema);
