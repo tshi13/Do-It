3d57247ebad280226fb1c21718aacfa9a5863998
Jonathan Bakhit <jbakhit1@jh.edu>
2022-10-02 17:47:49 -0400

    Added ability to update style of the chatbox just by passing as parameters!

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -32,8 +32,8 @@ export default class ChatBox extends Component {
 
     render() {
         return (
-            <div>
-                <Button variant = {this.state.variant ? "primary" : "outline-primary"} size = "lg" onClick={this.handleShow}>
+            <div style = {this.state.style}>
+                <Button variant = {this.state.variant ? "primary" : "outline-primary"} size = "lg" onClick={this.handleShow} style ={{backgroundColor: this.props.color}}>
                     Group Chat
                 </Button>
                 <Modal show={this.state.show} onHide={this.handleClose}>
