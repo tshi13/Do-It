d5ac021289454c2f60008f5134a23765e16fd7b7
Jonathan Bakhit <jbakhit1@jh.edu>
2022-10-07 22:24:35 -0400

    basic design for frontpage
    just worked on a cutesey design of the frontpage just bc I had time

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,13 +1,9 @@
 import React, { useState, useEffect } from "react";
 import ChatBox from "../components/ChatBox";
 import TaskModal from "../components/TaskModal";
-// import TaskModal from "../components/TaskModal";
 import '../styles/Home.css';
 import Database from "../utils/database";
-
-// const api = axios.create({
-//     baseURL: `http://localhost:5000/`,
-// });
+import frontpage from ".././assets/frontpage.png";
 
 
 export default function Home(props) {
@@ -28,7 +24,17 @@ export default function Home(props) {
 
     const homePage = (
         <div className="home">
-            <p>Home Page</p>
+            <div className="home__container">
+                    <div className="customContainer" style = {{backgroundColor: props.backgroundColor}}>
+                        <div style ={{float: 'left', width: '50%', marginTop: '10%', marginLeft: '10%'}}>
+                            <h1 className="h1" style ={{color: 'black', width: '25%'}}>Achieve Dreams!</h1>
+                            <h2 className="h2" style ={{color: 'black', width: '25%'}}>Please login to get started!</h2>
+                        </div>
+                        <div className="home__container__right" style ={{float: 'right', marginTop: '2%', marginRight: '2%'}}>
+                                <img src= {frontpage} alt="logo" />
+                        </div>
+                </div>
+            </div>
         </div>
     );
     return (
