73458aa637f4d46d5b460e31f16260fc48a9142c
Jonathan Bakhit <jbakhit1@jh.edu>
2022-10-17 15:14:32 -0400

    Update DisplayTasks.js

--- frontend/src/pages/DisplayTasks.js
+++ frontend/src/pages/DisplayTasks.js
@@ -10,70 +10,27 @@ const flexContainer = {
     padding: 0,
 };
 
-export default function DisplayTasks{
-    // const { tasks } = this.props;
-    constructor(props) {
-        super(props);
-        this.state = {
-            show: false,
-            message: "",
-            messages: [],
-            variant:  false,
-            tasks: props.privateTasks,
-            style: props.style,
-            userID: props.userID,
-            groupID: props.groupID,
-        };		
-    }
+export default function DisplayTasks(props){
 
-    componentDidMount () {
-        // this.getTasks = this.getTasks.bind(this);
-        this.props.getTasks();
-    }
+    const tasks = props.privateTasks;
 
-    handleChange = (event) => {
-        this.setState({message: event.target.value});
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        this.setState({messages: [...this.state.messages, this.state.message]});
-        this.setState({message: ""});
-    }
-
-	
-
-    // getTasks = () => {
-    //     taskDAO.getTasks({userID: this.state.userID})
-    //         .then((response) => {
-    //             this.setState({tasks: response});
-    //         })
-    // }
-
-    render() {
-        return (
-            <>
-                {/* <List style={ flexContainer }>
-                    {tasks.map((task, index) => {
-                        return <TaskCard task = {task} key = {index}/>
-                    })}
-                </List> */}
-                <Typography variant="h5" component="div" style={{"color": 'black', "margin": '2vw'}}>
-                    My Tasks
-                </Typography>
-                <Grid container spacing={1}>
-                    {
-                        this.props.privateTasks.map((task, index) => {
-                            return (
-                                <Grid item xs={2.2} key={index}>
-                                    <TaskCard key={index} task={task}/>
-                                </Grid>
-                            );
-                        })
-                    }
-                </Grid>
-                
-            </>
-        );
-    }
+    return (
+        <>
+            <Typography variant="h5" component="div" style={{"color": 'black', "margin": '2vw'}}>
+                My Tasks
+            </Typography>
+            <Grid container spacing={1}>
+                {
+                    tasks.map((task, index) => {
+                        return (
+                            <Grid item xs={2.2} key={index}>
+                                <TaskCard key={index} task={task}/>
+                            </Grid>
+                        );
+                    })
+                }
+            </Grid>
+            
+        </>
+    );
 }
