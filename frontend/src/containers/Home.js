4bba5c08c2c8ff770cd58a3f57e5cb52834b75a0
cli135 <licli019@gmail.com>
2022-10-19 15:28:20 -0400

    Add change that Jonathan suggested with tasks[0] null check

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -39,7 +39,9 @@ export default function Home(props) {
     useEffect(() => {
         taskDAO.getTasks({userID: props.userID})
             .then((tasks) => {
-                setPrivateTasks(tasks);
+                if (tasks[0]) {
+                    setPrivateTasks(tasks);
+                }
             })
     }, []);
     
