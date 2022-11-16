4b55f49f21bf1b3347d547a72e11990883e56dd0
cli135 <licli019@gmail.com>
2022-11-15 23:32:56 -0500

    prep for prod

--- frontend/src/utils/groupDAO.js
+++ frontend/src/utils/groupDAO.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 // Enable this for local development
-axios.defaults.baseURL = 'http://localhost:5000'; 
+// axios.defaults.baseURL = 'http://localhost:5000'; 
 
 //Enable this for heroku production app
-// axios.defaults.baseURL = 'https://backend-oose-doit.herokuapp.com/';
+axios.defaults.baseURL = 'https://backend-oose-doit.herokuapp.com/';
 
 async function getGroups(userID) {//This should not be here
     let res = await axios.get('/group/groups/' + userID).then(data => data);
